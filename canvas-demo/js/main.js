518e75bc85656586232ba777c9877cc4f799639a
soldiermakk <liangxia1998@outlook.com>
2019-04-16 09:15:48 +0800

    更新

--- canvas-demo/js/main.js
+++ canvas-demo/js/main.js
@@ -3,7 +3,8 @@ div.onmousedown = function(down){
     var x = down.clientX;
     var y = down.clientY;
     var divA = document.createElement('div');
-    divA.style = 'width=6px;height=6px;background:red;border-radius:3px;position:absolute;left:'+(x-3) +'px;right:'+(y-3) +'px;';
+    divA.className = 'divA';
+    divA.style = 'width:6px;height:6px;'+'background:red;border-radius:3px;'+'position:absolute;left:'+(x-3)+' px;'+'right:'+(y-3) +'px;';
     
     div.appendChild(divA)
 }
