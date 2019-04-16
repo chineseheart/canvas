9093a8e2e7c180c94c20b8cdea25d0813a38316f
soldiermakk <liangxia1998@outlook.com>
2019-04-16 21:06:18 +0800

    更新

--- canvas-demo/js/main.js
+++ canvas-demo/js/main.js
@@ -1,5 +1,45 @@
-var yyy = document.getElementById('xxx');
-var context = yyy.getContext('2d');
+var canvas = document.getElementById('canvas');
+var content = canvas.getContext('2d');
 
-context.fillstyle = 'red';
-context.fillRect(0,0,100,100);
+function drawCircle(x,y,radius){
+    content.beginPath();
+    content.arc(x,y,radius,0,Math.PI*2);
+    content.stroke();
+}
+
+function drawLine(x1,x2,y1,y2){
+    content.beginPath();
+    content.strokeStyle = 'bule'
+    content.moveTo(x1,x2)
+    content.lineTo(y1,y2)
+    content.stroke()
+    content.closePath()
+}
+var mouse = false;
+var lastPoint = {'x':undefined, 'y':undefined}
+
+canvas.onmousedown = function(aaa){
+    mouse = true;
+    var x = aaa.clientX;
+    var y = aaa.clientY;
+    drawCircle(x,y,5);
+    lastPoint = {'x':x, 'y':y}
+
+}
+canvas.onmousemove = function(aaa){
+    if (mouse){
+        var x = aaa.clientX;
+        var y = aaa.clientY;
+        drawCircle(x,y,5);
+        var lastPoint = {'x':undefined, 'y':undefined}
+        newPoint = {'x':x, 'y':y}
+        lastPoint = newPoint;
+        drawLine(lastPoint.x,lastPoint.y,lastPoint.x,lastPoint.y)
+
+
+    }
+
+}
+canvas.onmouseup = function(aaa){
+    mouse = false
+}
