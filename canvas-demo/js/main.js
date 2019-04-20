4737bd47bcb0508cb3fcb5811e5332578cfc15a2
soldiermakk <liangxia1998@outlook.com>
2019-04-20 14:58:54 +0800

    增加了画笔颜色

--- canvas-demo/js/main.js
+++ canvas-demo/js/main.js
@@ -4,7 +4,7 @@ var eraserEnabled = true;
 
 autuSetCanvasSize(canvas)
 
-listenToUser(canvas)
+
 
 eraser.onclick = function(){
     eraserEnabled = true
@@ -17,6 +17,72 @@ pen.onclick = function(){
     eraser.classList.remove('active')
 }
 
+red.onclick = function(){
+    content.fillStyle = '#ffb3a7'
+    content.strokeStyle = '#ffb3a7'
+    red.classList.add('active')
+    green.classList.remove('active')
+    yellow.classList.remove('active')
+    purple.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.move('active')
+
+}
+green.onclick = function(){
+    content.fillStyle = '#40de5a'
+    content.strokeStyle = '#40de5a'
+    green.classList.add('active')
+    red.classList.remove('active')
+    yellow.classList.remove('active')
+    purple.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.move('active')
+
+}
+yellow.onclick = function(){
+    content.fillStyle = '#faff72'
+    content.strokeStyle = '#faff72'
+    yellow.classList.add('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
+    purple.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.move('active')
+
+}
+purple.onclick = function(){
+    content.fillStyle = '#cca4e3'
+    content.strokeStyle = '#cca4e3'
+    purple.classList.add('active')
+    green.classList.remove('active')
+    yellow.classList.remove('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.move('active')
+
+}
+blue.onclick = function(){
+    content.fillStyle = '#3eede7'
+    content.strokeStyle = '#3eede7'
+    blue.classList.add('active')
+    green.classList.remove('active')
+    yellow.classList.remove('active')
+    purple.classList.remove('active')
+    red.classList.remove('active')
+    black.classList.move('active')
+
+}
+black.onclick = function(){
+    content.fillStyle = '#161823'
+    content.strokeStyle = '#161823'
+    black.classList.add('active')
+    green.classList.remove('active')
+    yellow.classList.remove('active')
+    purple.classList.remove('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+listenToUser(canvas)
 /****************/
 function listenToUser(canvas){
 
@@ -40,7 +106,7 @@ function listenToUser(canvas){
                 var x = aaa.touches[0].clientX;
                 var y = aaa.touches[0].clientY;
                 if(using){
-                    content.clearRect(x-7,y-7,14,14)
+                    content.clearRect(x-8,y-8,16,16)
                 }else if(mouse){
                     var newPoint = {'x':undefined, 'y':undefined}
                     newPoint = {'x':x, 'y':y}
@@ -59,7 +125,7 @@ function listenToUser(canvas){
             var y = aaa.clientY;
             if(eraserEnabled){
                 using = true
-                content.clearRect(x-5,y-5,10,10)
+                content.clearRect(x-8,y-8,16,16)
             }else{
                 mouse = true;
                 lastPoint = {'x':x, 'y':y}
@@ -86,7 +152,6 @@ function listenToUser(canvas){
 
 function drawLine(x1,y1,x2,y2){
     content.beginPath();
-    content.strokeStyle = 'black'
     content.moveTo(x1,y1)
     content.lineWidth = '6'
     content.lineTo(x2,y2)
@@ -95,7 +160,6 @@ function drawLine(x1,y1,x2,y2){
 }
 function drawCircle(x,y,radius){
     content.beginPath();
-    content.fillStyle = 'black'
     content.arc(x,y,radius,0,Math.PI*2);
     //content.stroke();
     content.fill()
